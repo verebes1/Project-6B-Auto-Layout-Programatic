98f6f1938f9d0d9e41de83304a83b4527dbd7af3
David Verbenyi <dverbenyi@gmail.com>
2018-06-18 22:49:22 +0100

    Initial Commit

---
+++ Project-6B-Auto-Layout-Programatic/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Project-6B-Auto-Layout-Programatic
+//
+//  Created by verebes on 18/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let label1: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .red
+        label.text = "THESE"
+        return label
+    }()
+    
+    let label2: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .cyan
+        label.text = "ARE"
+        return label
+    }()
+    
+    let label3: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .yellow
+        label.text = "SOME"
+        return label
+    }()
+    
+    let label4: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .green
+        label.text = "AWESOME"
+        return label
+    }()
+
+    let label5: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .orange
+        label.text = "LABELS"
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addSubViews()
+        setConstraints()
+
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    func addSubViews(){
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+    }
+    
+    func setConstraints(){
+        let viewsDictionary = ["label1":label1, "label2":label2, "label3":label3, "label4":label4, "label5":label5]
+        
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        
+        
+    }
+
+
+
+}
+
