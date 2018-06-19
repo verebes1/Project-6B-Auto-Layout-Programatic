f1646874d2bfc1d83eb2c48e356ca82d3bbad4b8
David Verbenyi <dverbenyi@gmail.com>
2018-06-19 09:34:58 +0100

    AutoLayout Anchors done

--- Project-6B-Auto-Layout-Programatic/ViewController.swift
+++ Project-6B-Auto-Layout-Programatic/ViewController.swift
@@ -55,7 +55,8 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         
         addSubViews()
-        setConstraints()
+        //setConstraintsVFL()
+        setConstraintsUsingAutolayoutAnchors()
 
     }
     
@@ -71,16 +72,32 @@ class ViewController: UIViewController {
         view.addSubview(label5)
     }
     
-    func setConstraints(){
+    func setConstraintsVFL(){
         let viewsDictionary = ["label1":label1, "label2":label2, "label3":label3, "label4":label4, "label5":label5]
         let metrics = ["labelHeight": 88]
         
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
+        //by default the - has 10 points space between items but you can make it zero -0- or 30 by -30-
         view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+    }
+    
+    func setConstraintsUsingAutolayoutAnchors() {
+        var previous: UILabel!
         
-        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if previous != nil {
+                //we have a previous label - create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor).isActive = true
+            }
+            
+        //set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
     }
 
 
